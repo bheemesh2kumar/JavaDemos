78a2b428d702a2fa1e718e89c713702a451c6d31
bheemesh2kumar <40527096+bheemesh2kumar@users.noreply.github.com>
2019-05-27 00:50:33 +0530

    Update TestDemo.java

--- src/AccessModifiers/TestDemo.java
+++ src/AccessModifiers/TestDemo.java
@@ -5,18 +5,18 @@ public class TestDemo {
 	
 	public void publicmethod1()
 	{
-		System.out.println("this is public method in same class");
+		System.out.println("this is public method in same public  class");
 	}
 	
 	private void privatemethod2()
 	{
-		System.out.println("this is private method in same class");
+		System.out.println("this is private method in same  private class");
 	}
 	
 	
 	protected void proteectedmethod3()
 	{
-		System.out.println("this is protected  method in same class");
+		System.out.println("this is protected  method in sameprotected class");
 	}
 	
 	void defaultmethod4()
